831d50f91edb42c569731745ea52073e17746f9e
gabrielcipriano <gabrielfcipriano@gmail.com>
2023-07-19 14:09:38 -0300

    test: custom aditional dispose and max size

--- src/Pool.test.ts
+++ src/Pool.test.ts
@@ -12,17 +12,63 @@ aPool('should be defined', () => {
 aPool('should auto recycle objects when using the "using" keyword', () => {
     const pool = new Pool(() => ({ a: 1, b: 2 }));
 
-    const objId = new WeakMap();
+    const wm = new WeakMap(); // to give each object a unique id
 
     if (true) { // new scope
         using obj = pool.get();
 
-        objId.set(obj, 'cipriano');
+        wm.set(obj, 1);
     }// dispose recycles the object
 
     using obj = pool.get();
 
-    assert.is(objId.get(obj), "cipriano");
+    assert.is(wm.get(obj), 1);
+});
+
+aPool('should auto recycle objects with a custom dispose method', () => {
+    const pool = new Pool(() => ({ 
+        prop: 0,
+        [Symbol.dispose]() { // we can use it to 'reset' the object when it is recycled
+            this.prop = 0;
+        }
+    }));
+
+    const wm = new WeakMap(); // to give the object a unique id
+
+    if (true) {
+        using obj = pool.get();
+
+        obj.prop = 1;
+
+        wm.set(obj, 2);
+    }
+
+    using obj = pool.get();
+
+    assert.is(wm.get(obj), 2);
+    assert.is(obj.prop, 0);
+});
+
+aPool('should be limited to a max size', () => {
+    const pool = new Pool(() => ({}), { maxSize: 1 });
+
+    const wm = new WeakMap(); // to give each object a unique id
+
+    if (true) {
+        using obj1 = pool.get();
+        using obj2 = pool.get();
+
+        wm.set(obj1, 1);
+        wm.set(obj2, 2);
+    }// dispose resolves as a stack. So obj2 is recycled, and obj1 is not due to the max size
+
+    using obj = pool.get();
+
+    assert.is(wm.get(obj), 2);
+
+    using otherObj = pool.get();
+
+    assert.is(wm.has(otherObj), false);
 });
 
 aPool.run();
